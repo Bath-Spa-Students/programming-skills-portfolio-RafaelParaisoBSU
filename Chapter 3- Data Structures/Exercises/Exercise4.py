8dc02db7e43f96a4c8b79551997df55eb299deaf
Rafael Paraiso <rafaelparaiso.bsu@gmail.com>
2023-10-22 21:00:37 +0400

    Exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,17 @@
+guests = ['Wayeth', 'Joshua', 'Andrei', 'Isaac', 'Paul']
+
+name = guests[0].title()
+print (f'{name}, would you like to join us for dinner at 8PM sharp tonight?')
+
+name = guests[1].title()
+print (f'{name}, do you mind joining us for dinner at 8PM sharp tonight?')
+
+name = guests[2].title()
+print (f'{name}, how does dinner sound at 8PM, Monday?')
+
+name = guests[3].title()
+print (f"{name}, theres a party at Rafael's place tonight, you coming?")
+
+name = guests[4].title()
+print (f"{name}, Have you heard of the party happening at Rafael's place at 8PM? Should we join them?")
+
