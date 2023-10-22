d79d1a17271c1d91b35c2c03c069b202dd80ad93
Rafael Paraiso <rafaelparaiso.bsu@gmail.com>
2023-10-22 21:00:31 +0400

    Exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,16 @@
+ModesTranspo = ['Yacht', 'Lamborghini', 'Private Jet', 'Helicopter', 'Bugatti',]
+
+msg = f'If I had to choose... I would love a {ModesTranspo[0]} and use it weekly.'
+print (msg)
+
+msg = f'If I had to choose... I would love a {ModesTranspo[1]} and use it daily.'
+print (msg)
+
+msg = f'If I had to choose... I would love a {ModesTranspo[2]} and use it monthly.'
+print (msg)
+
+msg = f'If I had to choose... I would love a {ModesTranspo[3]} and use it yearly.'
+print (msg)
+
+msg = f'If I had to choose... I would love a {ModesTranspo[4]} and use it every other day.'
+print (msg)
