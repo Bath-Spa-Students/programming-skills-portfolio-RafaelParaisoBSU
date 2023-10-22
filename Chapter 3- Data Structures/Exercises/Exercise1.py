69b276ee088698935ca9c3a6807ec59d12fc4994
Rafael Paraiso <rafaelparaiso.bsu@gmail.com>
2023-10-22 21:00:15 +0400

    Exercise1.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise1.py
@@ -0,0 +1,7 @@
+best_friends = ['Wayeth', 'Joshua','Isaac', 'Paul', 'Marc',]
+
+print(best_friends[0])
+print(best_friends[1])
+print(best_friends[2])
+print(best_friends[3])
+print(best_friends[4])
