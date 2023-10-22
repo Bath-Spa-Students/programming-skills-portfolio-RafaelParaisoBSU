2693f069f6eca906f4c4d06c5f5a82587cd446e4
Rafael Paraiso <rafaelparaiso.bsu@gmail.com>
2023-10-22 21:01:06 +0400

    Exercise7.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise7.py
@@ -0,0 +1,32 @@
+TouristSpots = ['Las Vegas, USA', 'Mt. Taal, Philippines', 'Burj Khalifa, U.A.E', 'Akihabara, Japan', 'Giza, Egypt']
+
+print ("In Original Order:")
+print (TouristSpots)
+
+print ("\nIn Alphabetical Order:")
+print (sorted(TouristSpots))
+
+print ("In Original Order:")
+print (TouristSpots)
+
+print ("\nIn Reverse Alphabetical Order:")
+print (sorted(TouristSpots, reverse=True))
+
+print ("In Original Order:")
+print (TouristSpots)
+
+print ("\nIn Reversed Order:")
+TouristSpots.reverse()
+print (TouristSpots)
+
+print ("In Original Order:")
+TouristSpots.reverse()
+print (TouristSpots)
+
+print ("\nIn Alphabetical:")
+TouristSpots.sort()
+print (TouristSpots)
+
+print ("In Reverse Alphabetical:")
+TouristSpots.sort(reverse=True)
+print (TouristSpots)
