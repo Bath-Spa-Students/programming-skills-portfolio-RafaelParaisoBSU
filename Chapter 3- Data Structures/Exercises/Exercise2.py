6912bcbfbdbf657ff4d8c2135e812ec132752176
Rafael Paraiso <rafaelparaiso.bsu@gmail.com>
2023-10-22 21:00:23 +0400

    Exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,17 @@
+best_friends = ['Wayeth', 'Joshua','Isaac', 'Paul', 'Marc',]
+
+greeting = f'Hello {best_friends[0]}, How are you doing?'
+print (greeting)
+
+greeting = f'Hey {best_friends[1]}!, What are you cooking?'
+print (greeting)
+
+greeting = f'Yo {best_friends[2]}!, What song are you playing?'
+print (greeting)
+
+greeting = f'Good morning {best_friends[3]}, How has your day been?'
+print (greeting)
+
+greeting = f'Woah {best_friends[4]}, is that what I think it is?'
+print (greeting)
+
