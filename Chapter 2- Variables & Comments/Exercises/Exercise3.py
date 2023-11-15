27c506b2973e5d4cf0ef0b134c0b2a09b53ee217
Rafael Paraiso <rafaelparaiso.bsu@gmail.com>
2023-11-15 18:46:42 +0400

    UpdatedExercises.py

--- Chapter 2- Variables & Comments/Exercises/Exercise3.py
+++ Chapter 2- Variables & Comments/Exercises/Exercise3.py
@@ -2,7 +2,10 @@
 
 name = ("\t Rafael Paraiso\n")
 print (name) #Unmodified Text
+
 print (name.lstrip()) #Using L Strip
+
 print (name.rstrip()) #Using R Strip
+
 print (name.strip()) #Using Strip
 
