8e8f57d687e5606dfa959011384c05fc392bf5f0
Rafael Paraiso <144213546+RafaelParaisoBSU@users.noreply.github.com>
2023-11-16 11:02:48 +0400

    Update Exercise3.py

--- Chapter 6- Loops/Exercises/Exercise3.py
+++ Chapter 6- Loops/Exercises/Exercise3.py
@@ -2,7 +2,7 @@
 
 infinite_loop = 0 # Stores a number into a variable.
 
-while infinite_loop <= str("test"): # Creates a while loop that infinitely increases the original value using an infinite float value.
+while infinite_loop <= float('inf'): # Creates a while loop that infinitely increases the original value using an infinite float value.
     print (infinite_loop)
     infinite_loop += 1 # Increases the original value of the variable by 1.
 
